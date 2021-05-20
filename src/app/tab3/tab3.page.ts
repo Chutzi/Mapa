ecbb900df050c1cd8484bcb39679a61bcc468371
Chutzi <chuygd123@gmail.com>
2021-05-19 22:01:36 -0500

    Perfil

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -10,11 +10,33 @@ import { Component, OnInit } from '@angular/core';
 export class Tab3Page implements OnInit{
 
   pic : Photo;
+  systemDark: MediaQueryList;
+
   constructor(public photo : CameraService) {
     this.photo.loadPhoto();
   }
   ngOnInit(): void {
+    
+    
+  }
 
+  onClick(event){
+    this.systemDark = window.matchMedia("(prefers-color-scheme: dark)");
+    this.systemDark.addListener(this.colorTest);
+    if(event.detail.checked){
+      document.body.setAttribute('data-theme', 'dark');
+    }
+    else{
+      document.body.setAttribute('data-theme', 'light');
+    }
+  }
+
+   colorTest(systemInitiatedDark) {
+    if (systemInitiatedDark.matches) {
+      document.body.setAttribute('data-theme', 'dark');		
+    } else {
+      document.body.setAttribute('data-theme', 'light');
+    }
   }
 
 
